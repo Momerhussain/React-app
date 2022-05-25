6bde0a0de3cec12705ead52cf445e475dcc5a3f1
omer <omer@Eocean1.com>
2022-05-25 16:32:57 +0500

    React app

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+
+    return (
+        <footer className='bg-dark text-light'>
+            <p className='text-center py-3'>
+            Copy rights &copy; Omer hussain
+            </p>
+        </footer>
+    )
+}
